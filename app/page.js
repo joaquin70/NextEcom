dc3d11199b9cb321ee819cb2ccdf603c9c2ee611
Joako <joako12409@gmail.com>
2024-03-18 11:17:56 -0300

    Update Boostrap.

--- app/page.js
+++ app/page.js
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
  return(
   <div>
-    <h1>Home</h1>
+    <h1 className="d-flex justify-content-center aling-items-center vh-100 text-uppercase">
+      Home
+      </h1>
   </div>
  );
 }
