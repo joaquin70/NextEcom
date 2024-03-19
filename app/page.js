c8ea7797e5ddf141aabce2f2424bf7f7a8f37fd3
Joako <joako12409@gmail.com>
2024-03-19 12:16:58 -0300

    Initializing Nav Y Routes.

--- app/page.js
+++ app/page.js
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 
 export default function Home() {
- return(
+  return (
+  <main>
   <div>
-    <h1 className="d-flex justify-content-center aling-items-center vh-100 text-uppercase">
-      Home
-      </h1>
+  <h1 className="d-flex justify-content-center align-items-center
+ vh-100 text-uppercase">
+  Home
+  </h1>
   </div>
- );
-}
+  </main>
+  );
+ }
