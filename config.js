592c0f1043ee1144305bd7722697f6d2e0ef24d6
Joako <joako12409@gmail.com>
2024-03-25 19:31:49 -0300

    .

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const DB_URI=
 "mongodb+srv://usuario:ek6yrarh3lBrEtPN@cluster0.kio9ahe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const API=
 process.env.NODE_ENV === "production" 
-? "https://xxx.vercel.com:3000/api"
+? "https://nextecom-joaquin-s-projects.vercel.app/api"
 :"http://localhost:3000/api";
 const NEXTAUTH_SECRET="SUDAFEWO938UR03294UR84390IU";
 module.exports = {
